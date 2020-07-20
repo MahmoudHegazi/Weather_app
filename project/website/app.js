058f7704ca18b244f518540ca65c6dcd987d6ec8
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-07-20 04:16:37 +0200

    Update app.js

--- project/website/app.js
+++ project/website/app.js
@@ -1,5 +1,15 @@
 /* Global Variables */
+const mytemp = document.getElementById("temp");
+const mydate = document.getElementById("date");
+const entery = document.getElementById("content");
+const img = document.getElementById("mimage");
+let sunny  = "msun.gif";
+let msunny = "sun.gif";
+let mcold = "vcold.gif";
+let vcold = "cold.gif";
+
 let wether = 'http://api.openweathermap.org/data/2.5/weather?id=3245&appid=db6608063a9d72758e29ea323da07bd1';
+//link for auto geo
 let geo = 'https://geolocation-db.com/json/0f761a30-fe14-11e9-b59f-e53803842572';
 
 // Create a new date instance dynamically with JS
@@ -8,26 +18,39 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 let settings = { method: "Get" };
 
+//geo 
+function xd(url, country, city) {
 
-// fetch url note we used fetch-node
-fetch(wether, settings)
+fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
-		//our json object has 1 property which is first
-        alert(json.main['temp']);
+        mytemp.innerText = "temp at " + city + ": " + json.main['temp'];
+        mydate.innerText = "You Are From: " + country;
+        entery.innerText = "Your City is: " + city;
+     if (json.main['temp'] >= 30) {
+         img.src = sunny;
+    } 
+    if (json.main['temp'] >= 16) {
+         img.src = msunny;
+    } 
+    
+    if (json.main['temp'] >= 0) {
+         img.src = mcold;
+    } 
+    if (json.main['temp'] < 0) {
+         img.src = vcold;
+    }
     });
+};
 
 
-//geo 
+
 fetch(geo, settings)
     .then(res => res.json())
     .then((json) => {
-		//our json object has 1 property which is first
-        alert(json.country_name);
-    });
+        let x = "http://api.openweathermap.org/data/2.5/weather?lat="+json.latitude+"&lon="+json.longitude+"&appid=db6608063a9d72758e29ea323da07bd1";
+        let country = json.country_name;
+        let city = json.city;
+        xd(x, country, city);
+});
 
-
-
-// your code goes here
-
-//	postData(wether);
