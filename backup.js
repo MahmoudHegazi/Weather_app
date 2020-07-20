2d247d25149862488ca65e46fb8c2ddcb896acc6
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-07-20 03:32:05 +0200

    Update backup.js

--- backup.js
+++ backup.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 const mytemp = document.getElementById("temp");
+const mydate = document.getElementById("date");
+const entery = document.getElementById("content");
 
 let wether = 'http://api.openweathermap.org/data/2.5/weather?id=3245&appid=db6608063a9d72758e29ea323da07bd1';
 //link for auto geo
@@ -12,12 +14,14 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 let settings = { method: "Get" };
 
 //geo 
-function xd(url) {
+function xd(url, country, city) {
 
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
-        mytemp.innerText = json.main['temp'];
+        mytemp.innerText = "temp at " + city + ": " + json.main['temp'];
+        mydate.innerText = "You Are From: " + country;
+        entery.innerText = "Your City is: " + city;
     });
 };
 
@@ -27,6 +31,8 @@ fetch(geo, settings)
     .then(res => res.json())
     .then((json) => {
         let x = "http://api.openweathermap.org/data/2.5/weather?lat="+json.latitude+"&lon="+json.longitude+"&appid=db6608063a9d72758e29ea323da07bd1";
-        xd(x);
+        let country = json.country_name;
+        let city = json.city;
+        xd(x, country, city);
 });
 
